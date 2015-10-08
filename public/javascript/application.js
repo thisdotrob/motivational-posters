5c0c7fb5c67d8258ba512b29f383f85f750755b4
thisdotrob <thisdotrob@gmail.com>
2015-10-08 19:34:51 +0100

    Last task

--- public/javascript/application.js
+++ public/javascript/application.js
@@ -6,7 +6,25 @@ $(document).on('click', '#search-results img', function() {
   $("#workspace img").remove();
   var img = $("<img>").attr('src', url);
   $("#workspace").append(img);
-})
+});
 $(document).on('input', '#text', function() {
   $("#caption").text($(this).val());
-})
+});
+$(document).on('change', '#left', function() {
+  $("#caption").css("left", $(this).val() + 'px');
+});
+$(document).on('change', '#top', function() {
+  $("#caption").css("top", $(this).val() + 'px');
+});
+$(document).on('change', '#width', function() {
+  $("#caption").css("width", $(this).val() + 'px');
+});
+$(document).on('change', '#size', function() {
+  $("#caption").css("font-size", $(this).val() + 'px');
+});
+$(document).on('change', '#colour', function() {
+  $("#caption").css("color", $(this).val());
+});
+$(document).on('change', '#align', function() {
+  $("#caption").css("text-align", $(this).val());
+});
